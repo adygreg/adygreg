eeff40f5b0400a4c7b4efc8ffd33a4710474c656
Adrian Fernandez <adygreg@mail.com>
2024-06-26 19:48:39 +0200

    add

--- src/pages/base/art.jsx
+++ src/pages/base/art.jsx
@@ -9,21 +9,27 @@ import drawing_8 from "/src/assets/images/drawing/Art.jpg"
 import drawing_9 from "/src/assets/images/drawing/Baka.jpg"
 import drawing_10 from "/src/assets/images/drawing/NYC.jpg"
 import drawing_11 from "/src/assets/images/drawing/Red.jpg"
+import drawing_12 from "/src/assets/images/drawing/Mountain.jpg"
+import drawing_13 from "/src/assets/images/drawing/Fwæh.jpg"
+import drawing_14 from "/src/assets/images/drawing/Sunset.jpg"
 
 function Art () {
     return(
         <div className="flex justify-center gap-5 flex-wrap w-full h-full">
+            <Tab img={drawing_14} title={"Sunset"} date={"24/06/24"} />
+            <Tab img={drawing_13} title={"Geometrics"} date={"24/06/24"} />
+            <Tab img={drawing_12} title={"Ranges"} date={"23/06/24"} />
             <Tab img={drawing_11} title={"Red"} date={"20/06/24"} />
             <Tab img={drawing_10} title={"Liberty"} date={"18/06/24"} />
             <Tab img={drawing_9} title={"Baka"} date={"18/06/24"} />
             {/* <Tab img={drawing_8} title={"Art"} date={"18/06/24"} /> */}
             <Tab img={drawing_7} title={"Lemon"} date={"15/06/24"} />
             <Tab img={drawing_6} title={"Miami"} date={"14/06/24"} />
-            {/* <Tab img={drawing_5} title={"Portrait"} date={"14/06/24"} /> */}
+            <Tab img={drawing_5} title={"Portrait"} date={"14/06/24"} />
             {/* <Tab img={drawing_4} title={"Space"} date={"13/06/24"} /> */}
             <Tab img={drawing_3} title={"Car"} date={"12/06/24"} />
             {/* <Tab img={drawing_2} title={"Landscape"} date={"11/06/24"} /> */}
-            {/* <Tab img={drawing_1} title={"Ornaments"} date={"10/06/24"} /> */}
+            <Tab img={drawing_1} title={"Ornaments"} date={"10/06/24"} />
         </div>
     );
 }
--- src/pages/base/design.jsx
+++ src/pages/base/design.jsx
@@ -3,9 +3,12 @@ import Tab from "../../components/tab";
 import igreg_off from "/src/assets/images/igreg/off.png"
 import igreg_on from "/src/assets/images/igreg/on.png"
 
+import collet from "/src/assets/images/collet/tbnail.png"
+
 function Design () {
     return(
         <div className="grid grid-cols-2 gap-4 [&>section]:h-[300px] w-full h-full">
+            {/* <Tab link={"/collet"} next_page={false} image={collet} image_2={collet} color={"#ffffff"} title={"Collet"} extra={""} extra_class={""}/> */}
             <Tab link={"/igreg"} next_page={false} image={igreg_off} image_2={igreg_on} color={"#bea383"} title={"iGreg"} extra={""} extra_class={""}/>
         </div>
     );
